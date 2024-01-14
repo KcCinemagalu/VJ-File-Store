1a5f09e3516cd1fb518aed38586261d952103778
KcCinemagalu <124599828+KcCinemagalu@users.noreply.github.com>
2024-01-14 21:03:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,9 +35,9 @@ API_HASH = environ.get("API_HASH", "cac21249a0c6373a1b742afb8dbc9cb7")
 ADMINS = int(environ.get("ADMINS", "5720092781"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://Sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://srgiconic:2Rsra8NQPdw2AgjT@cluster0.mscyobw.mongodb.net/?retryWrites=true&w=majority")
 CDB_NAME = environ.get("CDB_NAME", "clonevjbotz")
-DB_URI = environ.get("DB_URI", "mongodb+srv://pewam92589:zZ9qyzbpN0fGsoyp@cluster0.lg5ayam.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = environ.get("DB_URI", "mongodb+srv://Nandesha:alabal18@cluster0.6qjsxnq.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "vjbotz")
 
 # Don't Remove Credit Tg - @VJ_Botz
